1934fcb80e8ebaced6fd274129d648a89fb50654
Max Botwin <maxmarbot@gmail.com>
2021-10-26 13:06:27 -0500

    following along

--- server.js
+++ server.js
@@ -17,6 +17,23 @@ app.get('/', (req, res) => {
     rollbar.info('html file served successfully')
 })
 
+let students = []
+
+app.post('/api/student', (req, res) => {
+    const {name} = req.body 
+    name = name.trim()
+
+    students.push(name)
+    rollbar.log('Sudent added successfully', {author: 'Max', type: 'manual entry'})
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
+
 const port = process.env.PORT || 4545
+
+// app.get('/', (req, res) => {
+//     res.
+// })
 
 app.listen(port, () => console.log(`Activity detected in sector ${port}`))
